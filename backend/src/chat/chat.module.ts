97754d5169f9710af798894dcbef46a7d5d4223a
KunalMiddleware <kunal@middleware.io>
2022-03-14 19:53:19 +0530

    entity revert

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import Chat from './entities/chat.entity';
-import RoomUsers from './entities/room-users.entity';
-import ChatRoom from './entities/room.entity';
 import { ChatGateway } from './gateway/chat.gateway';
-
 @Module({
-  imports: [TypeOrmModule.forFeature([Chat, RoomUsers, ChatRoom])],
+  imports: [],
   providers: [ChatGateway],
 })
 export class ChatModule {}
